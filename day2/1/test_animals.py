31bb2d61e987ac4130f13506463baf6fb332654d
Nathalie Proos Vedin <nittilina@gmail.com>
2020-03-29 13:18:03 +0200

    Answers for day 2 exercises, part 1.

---
+++ day2/1/test_animals.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Import classes from your brand new package
+from animals import Mammals
+from animals import Birds
+
+# Create an object of Mammals class & call a method of it
+myMammal = Mammals()
+myMammal.printMembers()
+
+# Create an object of Birds class & call a method of it
+myBird = Birds()
+myBird.printMembers()
---
+++ day2/1/test_animals_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import animals
+
+m = animals.Mammals()
+m.printMembers()
+
+b = animals.Birds()
+b.printMembers()
